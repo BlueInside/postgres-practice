397688f40e6568a56abcbb7e556c216dcbf967e4
Karol <kpulawski13@gmail.com>
2024-09-14 11:02:00 +0100

    add query to delete users

--- db/queries.js
+++ db/queries.js
@@ -1,16 +1,22 @@
 const pool = require('./pool');
 
-async function getAllUsernames() {
-    const { rows } = await pool.query('SELECT * FROM usernames');
+async function getAllUsernames(query = '') {
+    const searchPattern = `%${query}%`;
+    const { rows } = await pool.query(`SELECT * FROM usernames WHERE username::TEXT ILIKE $1`, [searchPattern])
     return rows;
 }
 
-async function insertUsername(username) {
+async function deleteUsernames() {
+    await pool.query(`DELETE FROM usernames`)
+}
 
+
+async function insertUsername(username) {
     await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
 }
 
 module.exports = {
     getAllUsernames,
-    insertUsername
+    insertUsername,
+    deleteUsernames
 }
